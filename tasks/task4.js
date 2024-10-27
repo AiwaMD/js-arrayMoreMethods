d448e669aa436bde54e108a78ecfdc5c4dd1052b
AiwaMD <141828804+AiwaMD@users.noreply.github.com>
2024-10-27 11:00:11 +0200

    Update task4.js

--- tasks/task4.js
+++ tasks/task4.js
@@ -3,8 +3,15 @@
 // Якщо це так, то фільтруємо машини по бренду і повертаємо ті, що найбільш економні.
 
 function getMostFuelEfficientCars(cars, brand) {
-  // Ваш код
-}
+const allEfficient = cars.every(car => car.fuelConsumption < 6);
+
+  if (!allEfficient) {
+    return []; // Якщо не всі машини економні, повертаємо порожній масив
+  }
+
+  return cars
+    .filter(car => car.brand === brand)
+    .sort((a, b) => a.fuelConsumption - b.fuelConsumption);}
 
 // Приклад використання:
 const cars = [
